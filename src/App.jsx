02d5a5d59db0376f32f429bdb021230296dcc605
Ryan Greenup <ryanxgreenup@gmail.com>
2025-03-28 19:52:16 +1100

    initial commit flowbite solidjs template

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import styles from './App.module.css';
+import { Modal } from 'flowbite';
+
+
+function App() {
+  return (
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <img src={logo} class={styles.logo} alt="logo" />
+        <p>
+          Edit <code>src/App.jsx</code> and save to reload.
+        </p>
+        <a
+          class="text-4xl text-blue-600"
+          href="https://github.com/solidjs/solid"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Solid, Tailwind CSS and Flowbite
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
